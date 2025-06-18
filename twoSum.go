cbf6bb85d174f1436cb10f7e0f892ac05b03cb4f
MetaNodeTask <52944591+yidi6358@users.noreply.github.com>
2025-06-18 19:58:26 +0800

    Create twoSum.go

---
+++ twoSum.go
@@ -0,0 +1,11 @@
+/*两数之和*/
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
